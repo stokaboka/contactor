ea607b1ace5bc45b5fb2ff116282c3f5f247c497
igorkhorev <igorhorev@gmail.com>
2018-10-03 17:48:00 +0300

    add user service; add user-detail class; add user-detail mock data

--- src/app/api/users/user-detail.ts
+++ src/app/api/users/user-detail.ts
@@ -1,2 +1,19 @@
 export class UserDetail {
+  id: String;
+  age: Number;
+  city: String;
+  info: String;
+
+  constructor(
+    id: String,
+    age: Number,
+    city: String,
+    info: String
+  ) {
+    this.id = id;
+    this.age = age;
+    this.city = city;
+    this.info = info;
+  }
+
 }
--- src/app/api/users/users.service.ts
+++ src/app/api/users/users.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from './user';
+import { USERS } from './mock-users';
+import {UserDetail} from './user-detail';
+import {USERS_DETAILS} from './mock-user-detail';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
+  users: Observable<User[]>;
+  userDetails: Observable<UserDetail[]>;
+
   constructor() { }
+
+  init() {
+    this.users = this.getUsers();
+    this.userDetails = this.getUserDetails();
+  }
+
+  getUsers(): Observable<User[]> {
+    return of(USERS);
+  }
+
+  getUserDetails(): Observable<UserDetail[]> {
+    return of(USERS_DETAILS);
+  }
 }
