a28e447c81e77175f951ffcd01229f8370cbe3f9
igorkhorev <igorhorev@gmail.com>
2018-10-03 08:26:48 +0300

    add base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './ui/users/user/user.component';
+import { UsersComponent } from './ui/users/users/users.component';
+import { MessageComponent } from './ui/messages/message/message.component';
+import { MessagesComponent } from './ui/messages/messages/messages.component';
+import { UserEditorComponent } from './ui/users/user-editor/user-editor.component';
+import { MessageEditorComponent } from './ui/messages/message-editor/message-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UsersComponent,
+    MessageComponent,
+    MessagesComponent,
+    UserEditorComponent,
+    MessageEditorComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/ui/messages/message-editor/message-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-editor',
+  templateUrl: './message-editor.component.html',
+  styleUrls: ['./message-editor.component.css']
+})
+export class MessageEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
