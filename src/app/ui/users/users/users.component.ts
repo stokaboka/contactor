843255274feb512bb74b4429b2f15c3c1479c62b
igorkhorev <igorhorev@gmail.com>
2018-10-03 17:59:05 +0300

    modify User component - add user service

--- src/app/ui/users/users/users.component.ts
+++ src/app/ui/users/users/users.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { USERS } from '../../../api/users/mock-users';
 import {User} from '../../../api/users/user';
 
+import { UsersService } from '../../../api/users/users.service';
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -12,13 +14,23 @@ export class UsersComponent implements OnInit {
   users = USERS;
   user: User;
 
-  constructor() { }
+  constructor(
+    private usersService: UsersService
+  ) { }
 
   ngOnInit() {
+    this.init();
   }
 
   onClick(user: User): void {
     this.user = user;
   }
 
+  init(): void {
+    this.usersService.getUsers()
+      .subscribe(users => {
+        this.users = users;
+        this.user = this.users.length > 0 ? this.users[0] : null;
+      });
+  }
 }
